d52e2f385f873534da37cb7bb08867c42987039f
Amirhosein Rajabi <rajaby.amir@gmail.com>
2018-06-01 17:15:04 +0430

    add neo4jdriver and add product method

--- digi_crawler/src/main/java/Crawler.kt
+++ digi_crawler/src/main/java/Crawler.kt
@@ -1,12 +1,19 @@
 import kotlin.collections.*
 import kotlin.concurrent.thread
 import org.jsoup.Jsoup
+import org.neo4j.driver.v1.*
+import org.neo4j.driver.v1.Values.parameters
+import org.neo4j.driver.v1.exceptions.ServiceUnavailableException
 import java.util.concurrent.atomic.AtomicInteger
 import us.codecraft.xsoup.Xsoup
 import java.io.File
+import java.util.regex.Pattern
+import java.util.HashMap
 
-private class Crawler{
-    private var addressList = listOf<String>().toMutableList()
+
+
+private class Crawler {
+    private var addressList = mutableListOf<String>()
     private var startedJobs = AtomicInteger(0)
     private var pointer = AtomicInteger(1)
     private val maxDepth = 500_000
@@ -15,40 +22,47 @@ private class Crawler{
     private var stopped = false
     private var exited = false
     var startFrom = 0
-    init{
+    val neo4jInit = Neo4jDriver("bolt://localhost:7687","neo4j","qazWSXEDCRFV!1")
+    init {
         System.setProperty("javax.net.ssl.trustStore", "$basePath\\digi2.jks")
-        startFrom = try{
+        startFrom = try {
             val xz = sessionCounter
             val data = File(xz).readText()
             data.toInt()
-        }
-        catch (e: Exception){
+        } catch (e: Exception) {
             0
         }
-        for (i in startFrom..maxDepth){
+        for (i in startFrom..maxDepth) {
             addressList.add("http://www.digikala.com/Product/DKP-$i")
         }
+
     }
-    fun stop(){
+
+    fun stop() {
         stopped = true
-        while(!exited){
+        while (!exited) {
             Thread.sleep(2000)
         }
+        neo4jInit.close()
     }
-    fun addressCount():Int{
+
+    fun addressCount(): Int {
         return addressList.size
     }
-    fun currentCount(): Int{
+
+    fun currentCount(): Int {
         return pointer.get()
     }
-    private fun loadAddress(addressToGet:String):PageData{
+
+    private fun loadAddress(addressToGet: String): PageData {
         val jx = Jsoup.connect(addressToGet).get()
         val x = Xsoup.compile("//*[@id=\"frmSecProductMain\"]/div[2]/header/div[1]/h1/text()").evaluate(jx).get()
         val enTitle = Xsoup.compile("//*[@id=\"frmSecProductMain\"]/div[2]/header/div[1]/h1/span/text()").evaluate(jx).get()
         val price = Xsoup.compile("//*[@id=\"notifyMeButton\"]/@data-price").evaluate(jx).get()
         val brand = Xsoup.compile("//*[@id=\"frmSecProductMain\"]/div[2]/header/div/div/div[1]/a/text()").evaluate(jx).get()
         val image = Xsoup.compile("//*[@id=\"frmSecProductMain\"]/div[1]/div[2]/div/img/@src").evaluate(jx).get()
-        val description = Xsoup.compile("//*[@id=\"frmSecProductDescription\"]/div/div/p[2]/text()").evaluate(jx).get()
+        //val description = Xsoup.compile("//*[@id=\"frmSecProductDescription\"]/div/div/p[2]/text()").evaluate(jx).get()
+        val description = Xsoup.compile("//*[@id=\"frmSecProductDescription\"]/div/div/p/text()").evaluate(jx).get()
         val pd = PageData()
         pd.title = x
         pd.enTitle = enTitle
@@ -59,10 +73,11 @@ private class Crawler{
         return pd
     }
 
-    fun startCrawl(){
-        while(pointer.get() < addressCount() && !stopped){
-            if(pointer.get() % 10 == 0){
-                while (startedJobs.get() > 0){
+    fun startCrawl() {
+
+        while (pointer.get() < addressCount() && !stopped) {
+            if (pointer.get() % 10 == 0) {
+                while (startedJobs.get() > 0) {
                     Thread.sleep(5_000)
                 }
                 File(sessionCounter).writeText("${pointer.get()}")
@@ -72,13 +87,21 @@ private class Crawler{
                 try {
                     startedJobs.addAndGet(1)
                     val res = loadAddress(addressPointer)
+                    val product = Product()
+                    product.FaTitle = res.title
+                    product.EnTitle = res.enTitle
+                    product.Price = res.price
+                    product.ImagePath = res.imageData
+                    product.Description = res.description
+                    product.SourceURL = addressPointer
 
-                    println("Current Pointer : ${pointer.get()} and Address : $addressPointer >>> ${res.title}")
-                }
-                catch (e: Exception) {
+                    neo4jInit.addProduct(product)
+
+                    //println(res.description)
+                    println("Current Pointer : ${pointer.get()} and Address : $addressPointer >>> ${product.FaTitle}")
+                } catch (e: Exception) {
                     println(e.message)
-                }
-                finally {
+                } finally {
                     startedJobs.addAndGet(-1)
                 }
 
@@ -88,22 +111,50 @@ private class Crawler{
         exited = true
     }
 }
-fun main(args: Array<String>){
+
+class Neo4jDriver(uri: String, user: String, password: String) : AutoCloseable {
+    private var driver: Driver
+
+    init {
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password))
+    }
+
+    override fun close() {
+        driver.close()
+    }
+
+    fun addProduct(product: Product): Boolean {
+        try {
+            driver.session().use({ session ->
+                return session.writeTransaction(object : TransactionWork<Boolean> {
+                    override fun execute(tx: Transaction): Boolean {
+                        val query = "CALL dor.createProduct('${product.FaTitle}','${product.EnTitle}','${product.Description}',${product.Price},'${product.SourceURL}','${product.ImagePath}')"
+                        tx.run(query)
+                        return true
+                    }
+                })
+            })
+        } catch (ex: ServiceUnavailableException) {
+            return false
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+
     val x = Crawler()
     println("Welcome to DX Crawler!")
-    while(true){
+    while (true) {
         print("Tell : ")
         val c = readLine()
-        if(c=="stop"){
+        if (c == "stop") {
             x.stop()
             println("Bye Bye")
             break
-        }
-        else if(c=="state"){
+        } else if (c == "state") {
             println("Current State : ${x.currentCount()}")
-        }
-        else if(c=="start"){
-            thread{
+        } else if (c == "start") {
+            thread {
                 x.startCrawl()
             }
         }
--- digi_crawler/src/main/java/PageData.kt
+++ digi_crawler/src/main/java/PageData.kt
@@ -1,8 +1,20 @@
-class PageData{
+class PageData {
     var title: String? = ""
-    var enTitle : String?= ""
-    var price : String? = ""
+    var enTitle: String? = ""
+    var price: String? = ""
     var brand: String? = ""
     var description: String? = ""
     var imageData: String? = ""
+}
+
+class Product {
+    var FaTitle: String? = ""
+    var EnTitle: String? = ""
+    var HashTitle: String? = ""
+    var Price: String? = ""
+    var ImagePath: String? = ""
+    var Description: String? = ""
+    var SourceURL: String? = ""
+    var CreatedDate = null
+
 }
